fa1bf4844c5869862e09d50b41457c3c203fc793
SlamTheDragon <derfeljohn2002@gmail.com>
2022-03-28 15:56:57 +0800

    Update questions.java

--- guessTheWord/ProgramData/questions.java
+++ guessTheWord/ProgramData/questions.java
@@ -2,8 +2,10 @@ package guessTheWord.ProgramData;
 
 public class questions {
     private static final String[] QPlace = new String[] {
-        "Q1",
-        "Q2"
+        "It is a country that comprises about 7,460 islands that are divided into three primary divisions from north to south.",
+        "What majestic mountain range is found in Switzerland?",
+        "This is the capital city of the Philippines.",
+        "dude wtf, your questions are hard"
     };
 
     private static final String[] QThings = new String[] {
